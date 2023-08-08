7257034f781a78381aed0da07e26f746cd078d7c
fardinabir <fardin.abir@pathao.com>
2023-08-08 12:53:23 +0600

    Included go routine for executing concurrent repo details fetch from github API

--- finders/finders.go
+++ finders/finders.go
@@ -11,17 +11,24 @@ import (
 	"os"
 	"star-seeker/models"
 	"strings"
+	"sync"
+	"time"
 )
 
 func FindRepos(keywords []string) {
+
+	timeStart := time.Now()
+
 	ctx := context.Background()
 	name := strings.Join(keywords, " ")
 	site := "github.com"
 
 	result, _ := googlesearch.Search(ctx, name+" site:"+site)
+	fmt.Println("Google Search Ended...Total... ", len(result))
 	repoUrls := processSearchResult(&result)
 	prepareRow(repoUrls)
 
+	fmt.Println("-------Total Exec Time : --------- : ", time.Now().Sub(timeStart))
 }
 
 func processSearchResult(result *[]googlesearch.Result) []string {
@@ -33,9 +40,7 @@ func processSearchResult(result *[]googlesearch.Result) []string {
 			continue
 		}
 		repoUserSuffix := trimRepoUser(url[19:len(url)])
-		//fmt.Println(url)
 		newApiUrl := "https://api.github.com/repos/" + repoUserSuffix
-		//fmt.Println(newApiUrl)
 		repoUrls = append(repoUrls, newApiUrl)
 		uniqueUrls[url] = true
 	}
@@ -48,29 +53,44 @@ func prepareRow(repoUrls []string) {
 	table.SetAlignment(tablewriter.ALIGN_CENTER)
 	//table.SetAutoFormatHeaders(false)
 	//table.SetAutoWrapText(false)
+	fmt.Println("Search Started, Preparing Table")
+	var wg sync.WaitGroup
 	for _, newApiUrl := range repoUrls {
-		resp, err := http.Get(newApiUrl)
-		if err != nil {
-			fmt.Println("Error in Getting Repo")
-		}
-		body, err := io.ReadAll(resp.Body)
-		//fmt.Println(resp.StatusCode, "---------------------------------------------------------------------")
-		if resp.StatusCode == 200 {
-			var respRepo models.RepoDetails
-			err = json.Unmarshal(body, &respRepo)
-			if err != nil {
-				fmt.Println("Error while unmarshalling")
-			}
-			if respRepo.FullName != "" {
-				shortRepoDetails := respRepo.GetShortDetails()
-				table.Append(shortRepoDetails)
-				//fmt.Println(" https://github.com/"+respRepo.FullName, "\t>>>>>>>>>>>>>>>\t", respRepo.StargazersCount)
-			}
-		}
+		wg.Add(1)
+		i := newApiUrl
+
+		go func() {
+			defer wg.Done()
+			GetRepoDetails(i, table)
+		}()
 	}
+	wg.Wait()
+
+	fmt.Println("Search Fininshed, Rendering Table")
 	table.Render()
 }
 
-func formatRow(repo models.RepoDetailsShort) {
-
+func GetRepoDetails(newApiUrl string, table *tablewriter.Table) {
+	resp, err := http.Get(newApiUrl)
+	if err != nil {
+		fmt.Println("Error while Getting Repos Info")
+		return
+	}
+	body, err := io.ReadAll(resp.Body)
+	//if resp.StatusCode != 200 {
+	//	fmt.Println(resp.StatusCode, "---------------GotUnexpectedResult-------------------", resp)
+	//}
+	if resp.StatusCode == 200 {
+		var respRepo models.RepoDetails
+		err = json.Unmarshal(body, &respRepo)
+		if err != nil {
+			fmt.Println("Error while unmarshalling")
+			return
+		}
+		if respRepo.FullName != "" {
+			shortRepoDetails := respRepo.GetShortDetails()
+			table.Append(shortRepoDetails)
+			//fmt.Println(" https://github.com/"+respRepo.FullName, "\t>>>>>>>>>>>>>>>\t", respRepo.StargazersCount)
+		}
+	}
 }
